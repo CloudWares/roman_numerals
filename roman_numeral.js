8e1731e9df3f255bffe6c56fd1204b20527576ab
CloudWares <hugo.fa.slv@gmail.com>
2016-08-29 17:49:52 +0000

    BugFix in padder function

--- roman_numeral.js
+++ roman_numeral.js
@@ -1,20 +1,22 @@
 function convertToRoman(num) {
   var ans = "";
   
-  var dictionary = [["I", "V"], ["X", "L"], ["C", "D"], ["M"]]
+  var dictionary = [["I", "V"], ["X", "L"], ["C", "D"], ["M"]];
    
   var unit = parseInt(num % 10);
   var tens = parseInt((num % 100) / 10);
   var hundreds = parseInt((num % 1000) / 100);
   var thousands = parseInt(num / 1000);
 
-  var number = [unit, tens, hundreds];   
-  var ans = [];
+  var number = [unit, tens, hundreds];  
 
   var padder = function(str, n) {
   	if (n > 1) {
   		return str + padder(str, n - 1);
   	}
+    if (n <= 0) {
+      return "";
+    }
   	return str;
   };
 
@@ -38,7 +40,7 @@ function convertToRoman(num) {
   		ans += (number[i] % 2) ? dictionary[i + 1][0] : dictionary[i][1];
   	}
 
-  };
+  }
 
   return ans;
 }
